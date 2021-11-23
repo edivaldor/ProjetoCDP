610df055b04cd27c7d8a1ec0f27b50218604b51c
edivaldor <edivaldor@gmail.com>
2021-11-22 22:34:22 -0300

    Adição de campo nome

--- documentos/models.py
+++ documentos/models.py
@@ -5,13 +5,11 @@ from django.db import models
 class DocumentosPregao(models.Model):
     nome=models.CharField(max_length=200,blank=True)
     cpf=models.CharField(max_length=18,blank=True)
-    contrato_social = models.FileField(blank=True, upload_to='files')
+    contrato_social = models.FileField(null=True, blank=True, upload_to='files')
     procuracao_IPP = models.FileField(blank=True, upload_to='files')
     termo_de_credenciamento = models.FileField(blank=True, upload_to='files')
     declaracao_de_PARH = models.FileField(blank=True, upload_to='files')
     declaracao_de_MEPP = models.FileField(blank=True, upload_to='files')
-
-
 
 
 
